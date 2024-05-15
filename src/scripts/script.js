daf5c336dc9231edb18ea79070abf6aaacfc651e
antonioabate11 <115202005+antonioabatte@users.noreply.github.com>
2024-05-15 17:47:24 -0300

    Home Alpha 0.1 Complete 👌
    New Script
    Brief News
    Footer "Animation"
    🫡

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -15,6 +15,9 @@ document.addEventListener('DOMContentLoaded', function() {
         dataElement.textContent = data;
         imagemElement.src = imagemSrc;
 
+        var LastNDiv = document.querySelector('.LastN');
+        LastNDiv.appendChild(divClone);
+
         divClone.style.display = 'block';
 
         divClone.style.position = 'absolute';
@@ -45,6 +48,15 @@ document.addEventListener('DOMContentLoaded', function() {
         '37%',
     );
 
+    clonarNoticia(
+        'Governo Biden envia ao Congresso dos EUA pacote de US$ 1 bilhão em armas a Israel',
+        'g1',
+        'Mai. 14, 2024',
+        './src/assets/biden.jpg',
+        '56%',
+        '37%',
+    )
+    
 });
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -101,4 +113,60 @@ document.addEventListener('DOMContentLoaded', function() {
         './src/assets/justica.jpg'
     );
 
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    function clonePapersLarge(titulo, autor, data, descricao, top, left) {
+        var divOriginal = document.querySelector('.PapersLarge');
+        var divClone = divOriginal.cloneNode(true);
+
+        var tituloElement = divClone.querySelector('.h10');
+        var autorElement = divClone.querySelector('.Autor3');
+        var dataElement = divClone.querySelector('.Data3');
+        var descricaoElement = divClone.querySelector('.descricao3');
+
+        tituloElement.textContent = titulo;
+        autorElement.textContent = autor;
+        dataElement.textContent = data;
+        descricaoElement.textContent = descricao;
+
+        divClone.style.display = 'block';
+        divClone.style.position = 'absolute';
+        divClone.style.top = top;
+        divClone.style.marginLeft = left;
+
+        document.body.appendChild(divClone);
+    }
+
+    clonePapersLarge(
+        'Robert Fico, primeiro-ministro da Eslováquia, é baleado na rua; estado de saúde é crítico, com risco de vida',
+        'g1',
+        'Mai. 15, 2024',
+        'Cinco tiros foram disparados após uma reunião de governo, sendo que um deles atingiu o premiê no abdômen, disse a rede de TV TA3. Suspeito tem 71 anos. Robert Fico foi transferido de helicóptero para hospital da capital, Bratislava, e se encontra em estado crítico.',
+        '141%',
+        '10%',
+    );
+
+    clonePapersLarge(
+        'Biden e Trump concordam em fazer 1º debate em junho',
+        'g1',
+        'Mai. 15, 2024',
+        'O presidente Joe Biden e o ex-presidente Donald Trump concordaram na quarta-feira em realizar dois debates de campanha em junho e setembro, mas não havia garantias de que isso aconteceria, já que seus campos pareciam distantes em detalhes importantes, como o cenário e as regras básicas para o potencial confronto. desligados.',
+        '184%',
+        '10%'
+    )
+});
+
+window.addEventListener('scroll', function() {
+    var footer = document.querySelector('footer');
+    var windowHeight = window.innerHeight;
+    var scrollY = window.scrollY || window.pageYOffset;
+    var documentHeight = document.body.clientHeight;
+    var triggerHeight = 50; // Ajuste conforme necessário
+
+    if (windowHeight + scrollY >= documentHeight - triggerHeight) {
+        footer.classList.add('visible');
+    } else {
+        footer.classList.remove('visible');
+    }
 });
